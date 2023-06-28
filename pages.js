a7dc84c2f6046918bce59cd55327d241e13620f5
rheakher <rheakher@gmail.com>
2023-06-29 00:02:48 +0100

    Initial Commit

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/rheakher/sayali.github.io.git";
+const repoURL = "https://github.com/rheakher/rheakher.github.io.git";
 
 ghpages.publish(
   pathname,
