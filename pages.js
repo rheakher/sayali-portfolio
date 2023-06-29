fe203708eb179f58e52525d6329b05b424765bf5
rheakher <rheakher@gmail.com>
2023-06-29 23:58:06 +0100

    Initial Commit

--- pages.js
+++ pages.js
@@ -1,15 +1,30 @@
 const ghpages = require("gh-pages");
-const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/rheakher/rheakher.github.io.git";
+const build_path = './build';
 
+const username = "rheakher";
+const repoURL = "https://github.com/rheakher/rheakher.github.io.git";
+const branch = "master";
+
+/**
+ * Deploy the built site to GitHub Pages with custom settings and options
+ * [username] is the GitHub username of the user who owns the repo
+ * [repoURL] - The URL of the GitHub repository to deploy to
+ * [branch] - The branch to deploy to
+ * [build_path] - The path to the built site
+ *
+ * node scripts/pages.js <username> <branch>
+ * i.e.
+ * node scripts/pages.js hashirshoaeb master
+ */
 ghpages.publish(
-  pathname,
+  build_path,
   {
-    branch: "master",
+    branch: branch,
     repo: repoURL,
+    dotfiles: true,
   },
   (err) => {
-    if (err) console.log("ERROR: ", err);
-    else console.log("PUBLISHED");
+    if (err) console.error("ERROR: ", err);
+    else console.log("PUBLISHED via gh-pages");
   }
 );
